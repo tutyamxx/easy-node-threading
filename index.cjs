485a00799d9b024e1613ad423f5c97d54b62ef4d
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-14 10:06:24 +0000

    UPDATE: Version change to trigger CI

--- index.cjs
+++ index.cjs
@@ -1,6 +1,6 @@
 /**
  *  easy-node-threading - ⚡ Run JavaScript functions or files in isolated Node.js worker threads with a single call. Simple, minimal, and modern.
- *  @version: v1.0.0
+ *  @version: v1.0.1
  *  @link: https://github.com/tutyamxx/easy-node-threading
  *  @license: MIT
  **/
